09001f308ab8b750e9e9b31a768d81b415bb2df1
Kepins <sz.kepinski1@gmail.com>
2023-04-03 21:49:28 +0200

    Laboratory done

--- labapp3/app/src/main/java/com/example/labapp3/MainActivity.kt
+++ labapp3/app/src/main/java/com/example/labapp3/MainActivity.kt
@@ -1,17 +1,19 @@
 package com.example.labapp3
 
 import android.content.Intent
+import android.content.SharedPreferences
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
+import android.widget.*
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
     private lateinit var db: SQLiteDatabase
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,6 +26,8 @@ class MainActivity : AppCompatActivity() {
         addEntryButton.setOnClickListener{
             onAddEntryButtonClick()
         }
+        updateTextView()
+        updateListView()
     }
 
 
@@ -38,9 +42,50 @@ class MainActivity : AppCompatActivity() {
         {
             data?.let {
                 val returnedStringValue: String = data.getStringExtra("value")!!
+                db.execSQL("INSERT INTO lab3(data) VALUES ('%s');".format(returnedStringValue))
 
+                val sdf = SimpleDateFormat("dd.MM.yyyy hh:mm:ss")
+                val currentDate: String = sdf.format(Date())
+
+                val preferences: SharedPreferences = getSharedPreferences("db-preferences", MODE_PRIVATE)
+                val editor: SharedPreferences.Editor = preferences.edit()
+                editor.putString("mod-date", currentDate)
+                editor.apply()
+
+                updateTextView()
+                updateListView()
             }
         }
         super.onActivityResult(requestCode, resultCode, data)
+    }
+
+
+    fun updateListView(){
+        val arrayList: MutableList<String> = ArrayList();
+        val arrayAdapter: ArrayAdapter<String> = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList)
+        val listView: ListView = findViewById(R.id.listView)
+        val c: Cursor = db.rawQuery("SELECT data FROM lab3", null)
+        while(c.moveToNext())
+            arrayList.add(c.getString(0))
+        c.close()
+        listView.adapter = arrayAdapter
+    }
+
+    fun updateTextView() {
+        val preferences: SharedPreferences = getSharedPreferences("db-preferences", MODE_PRIVATE)
+
+        val textView: TextView = findViewById(R.id.textView)
+        val modDate: String? = preferences.getString("mod-date", null)
+
+        modDate?.let {
+            textView.text = modDate
+        } ?: run{
+            textView.text = "Never modified!"
+        }
+    }
+
+    override fun onDestroy() {
+        db.close()
+        super.onDestroy()
     }
 }
